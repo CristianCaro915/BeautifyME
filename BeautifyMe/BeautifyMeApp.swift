593c39b0a5371818a3b7d1380328a314100760af
CristianCaro915 <c.caro@uniandes.edu.co>
2024-09-09 20:25:30 -0500

    Initial commit

---
+++ BeautifyMe/BeautifyMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeautifyMeApp.swift
+//  BeautifyMe
+//
+//  Created by Cristian Caro on 9/09/24.
+//
+
+import SwiftUI
+
+@main
+struct BeautifyMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
